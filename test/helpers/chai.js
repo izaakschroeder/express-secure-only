a060de0556a8234f60141b67890c8b898b18f8ee
Izaak Schroeder <izaak.schroeder@gmail.com>
2014-11-14 22:52:15 -0800

    Fix chai helper.

--- test/helpers/chai.js
+++ test/helpers/chai.js
@@ -3,8 +3,6 @@
 
 var chai = require('chai');
 
-chai.use(require('sinon-chai'));
-chai.use(require('chai-things'));
 chai.use(require('supertest-chai').httpAsserts);
 
 global.expect = chai.expect;
